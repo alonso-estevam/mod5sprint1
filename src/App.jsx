0f0fa21a2bd1ead4c7376b7a782ed9860dbd2c11
alonso-estevam <alonso_estevam@outlook.com>
2022-08-19 16:05:40 -0300

    feat: add article component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Article from './components/Article/Article'
 import Header from './components/Header/Header'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Article />
     </div>
   )
 }
