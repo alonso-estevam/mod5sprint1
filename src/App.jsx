6661a747b5058312dbcacda058ed4e72ce96b1d9
alonso-estevam <alonso_estevam@outlook.com>
2022-08-19 17:08:38 -0300

    feat: import and populate product card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Article from './components/Article/Article'
 import Header from './components/Header/Header'
+import ProductCard from './components/ProductCard/ProductCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,16 @@ function App() {
     <div className="App">
       <Header />
       <Article />
+
+      <main>
+        <ProductCard name="Nome do produto 1" description="Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata." listPrice="De: R$ 23,99" salePrice="Por: R$ 19,99" installment="ou 2x de R$ 9,99"/>
+
+        <ProductCard name="Nome do produto 1" description="Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata." listPrice="De: R$ 23,99" salePrice="Por: R$ 19,99" installment="ou 2x de R$ 9,99"/>
+
+        <ProductCard name="Nome do produto 1" description="Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata." listPrice="De: R$ 23,99" salePrice="Por: R$ 19,99" installment="ou 2x de R$ 9,99"/>
+
+        <ProductCard name="Nome do produto 1" description="Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata." listPrice="De: R$ 23,99" salePrice="Por: R$ 19,99" installment="ou 2x de R$ 9,99"/>
+      </main>
     </div>
   )
 }
