b1be13af1f184d10c2c951d97e911b704f2974c0
alonso-estevam <alonso_estevam@outlook.com>
2022-08-19 15:37:15 -0300

    feat: add header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h3>uma seleção de produtos</h3>
+        <h1>especial para você</h1>
+        <h2>Todos os produtos dessa lista foram selecionados a partir da sua navegação. Aproveite!</h2>
+    </div>
+  )
+}
+
+export default Header
